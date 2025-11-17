646c6f0bab01ce3e96c5b1f0fb85b69fffae1d30
Sunny Yadav <yadav.sunny@northeastern.edu>
2025-11-17 15:26:57 -0500

    Base project created

---
+++ FitQuest/FitQuest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitQuest
+//
+//  Created by Sunny Yadav on 11/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FitQuest/FitQuestTests/FitQuestTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitQuestTests.swift
+//  FitQuestTests
+//
+//  Created by Sunny Yadav on 11/17/25.
+//
+
+import Testing
+@testable import FitQuest
+
+struct FitQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
